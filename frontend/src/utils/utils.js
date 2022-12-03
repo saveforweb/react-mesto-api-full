49baa5a57baa44dd9101570a9928b4c402855b8c
Anton Popov <anton@bbbro.ru>
2022-12-03 12:48:27 +0300

    fix api url

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,13 +1,20 @@
+// export const apiConfig = {
+//   baseUrl: "https://mesto.nomoreparties.co/v1/cohort-48",
+//   headers: {
+//     authorization: "9f24a9a8-78aa-432d-bb8f-93b592333388",
+//     "Content-Type": "application/json",
+//   }
+// }
+
 export const apiConfig = {
-  baseUrl: "https://mesto.nomoreparties.co/v1/cohort-48",
+  baseUrl: "http://anton.nomoredomains.club/api",
   headers: {
-    authorization: "9f24a9a8-78aa-432d-bb8f-93b592333388",
     "Content-Type": "application/json",
   }
 }
 
 export const apiAuthConfig = {
-  baseUrl: "https://auth.nomoreparties.co",
+  baseUrl: "http://anton.nomoredomains.club/api",
   headers: {
     "Content-Type": "application/json",
   }
