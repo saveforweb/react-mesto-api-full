05bbf6c023cc55f6fbb16aaaf8479738ed33fabb
Anton Popov <anton@bbbro.ru>
2022-12-03 16:50:29 +0300

    fix

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -6,11 +6,8 @@ const { tokenString = 'dev-secret' } = process.env;
 module.exports = (req, res, next) => {
   const token = req.cookies.jwt;
 
-  res.send({ req });
-  res.end();
-
   if (!token) {
-    throw new errorsList.UnauthorizedError('Необходима авторизация.');
+    throw new errorsList.UnauthorizedError(`Необходима авторизация.${req.baseUrl}`);
   }
 
   let payload;
