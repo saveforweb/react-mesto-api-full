c0e5f79035de82f7387669796c1ee56c3f8bc4f9
Anton Popov <anton@bbbro.ru>
2022-12-04 18:05:55 +0300

    fix

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -70,6 +70,7 @@ function App() {
   function handleCardLike(card) {
     console.log(card);
     console.log(currentUser._id);
+    console.log(card._id);
 
     const isLiked = card.likes.some(i => i._id === currentUser._id);
 
