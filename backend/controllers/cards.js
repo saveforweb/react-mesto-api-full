317873ab7bda09f5c0e72814d6c88b2a5f16102f
Anton Popov <anton@bbbro.ru>
2022-12-06 17:13:37 +0300

    fix trash button

--- backend/controllers/cards.js
+++ backend/controllers/cards.js
@@ -15,6 +15,7 @@ module.exports.createCard = (req, res, next) => {
   const { name, link } = req.body;
 
   Card.create({ name, link, owner })
+    .populate(['owner', 'likes'])
     .then((card) => res.send({ data: card }))
     .catch((err) => {
       if (err.name === 'ValidationError') {
